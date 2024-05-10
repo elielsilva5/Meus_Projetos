3c7524b77630c2bf4ab985000b191ce0189d5460
elielsilva5 <elielsilva.eng@gmail.com>
2024-05-09 22:52:14 -0300

    Update main.py

--- Projeto_python/main.py
+++ Projeto_python/main.py
@@ -10,3 +10,9 @@ elif salario > 6000 and salario <= 15000:
     print('Programador senior')
 else:
     print('Gerente de projetos')
+
+listas_numeros = [1, 2, 3]
+
+print(listas_numeros[0])
+print(listas_numeros[1])
+print(listas_numeros[2])
