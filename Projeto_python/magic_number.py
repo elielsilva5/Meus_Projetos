0f13918316338eed22fffa2b799eea21362f29ca
elielsilva5 <elielsilva.eng@gmail.com>
2024-05-10 10:24:46 -0300

    Create magic_number.py

---
+++ Projeto_python/magic_number.py
@@ -0,0 +1,7 @@
+'''Intuido do projeto:
+Desenvolver uma calculadora que mostra quantos fundos imobiliários será necessário para conseguir um novo fundo sem ter que comprar com recursos próprios'''
+
+# perquisar as bibliotecas em python necessárias para execução do projeto
+# fazer a lógica da calculadora
+# desenvolver o código
+# manter as cotas atualizadas 
